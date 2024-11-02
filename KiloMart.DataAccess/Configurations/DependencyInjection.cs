eaf2773de7a24751f218f3e69f0abf80456e7236
mohamad.alturky <m799678@gmail.com>
2024-11-02 03:32:21 +0300

    adding api and Dbfactory

---
+++ KiloMart.DataAccess/Configurations/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using KiloMart.DataAccess.Contracts;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace KiloMart.DataAccess.Configurations;
+
+public static class DependencyInjection
+{
+    public static void AddDataAccess(this IServiceCollection services, IConfiguration configuration)
+    {
+
+        services.AddSingleton<IDbFactory>(_ =>
+        {
+            return new DbFactory(configuration.GetConnectionString("DefaultConnection")!);
+        });
+    }
+}
---
+++ KiloMart.DataAccess/Contracts/DbFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.SqlClient;
+using SqlKata.Compilers;
+using SqlKata.Execution;
+using System.Data;
+
+namespace KiloMart.DataAccess.Contracts;
+
+public class DbFactory(string connectionString) : IDbFactory
+{
+
+    public IDbConnection CreateDbConnection()
+    {
+        return new SqlConnection(connectionString);
+    }
+
+    public QueryFactory CreateSqlQuery()
+    {
+        var connection = new SqlConnection(connectionString);
+        var compiler = new SqlServerCompiler();
+        return new QueryFactory(connection, compiler);
+    }
+}

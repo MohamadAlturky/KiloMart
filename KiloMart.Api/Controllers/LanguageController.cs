ea348075eaf25a94fb9fc4d1ef026ea583bf5b96
mohamad.alturky <m799678@gmail.com>
2024-11-03 02:47:27 +0300

    adding language controller

---
+++ KiloMart.Api/Controllers/LanguageController.cs
@@ -0,0 +1,35 @@
+﻿using KiloMart.Domain.Requests;
+using KiloMart.Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KiloMart.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LanguageController : ControllerBase
+    {
+        [HttpPost("add")]
+        public IActionResult AddLanguage([FromBody] AddLanguageRequest request)
+        {
+            // Validate the request model
+            var (isValid, errors) = request.Validate();
+
+            if (!isValid)
+            {
+                // Return BadRequest with validation errors if the model is invalid
+                return BadRequest(errors);
+            }
+
+            // Call the service method
+            var response = LanguageService.AddLanguage(request);
+
+            // Return the appropriate response
+            if (response.Success)
+            {
+                return Ok(response);
+            }
+
+            return BadRequest(new List<string> { response.Message });
+        }
+    }
+}
---
+++ KiloMart.Domain/Responses/AddLanguageResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace KiloMart.Domain.Responses;
+
+public class AddLanguageResponse
+{
+    public bool Success { get; set; }
+    public string Message { get; set; }
+}

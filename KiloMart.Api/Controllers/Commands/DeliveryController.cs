9eaf32a85fdede5275345a83f7b2595b146dc0d1
mohamad.alturky <m799678@gmail.com>
2024-11-08 22:05:04 +0300

    adding delivery profile

--- KiloMart.Api/Controllers/Commands/DeliveryController.cs
+++ KiloMart.Api/Controllers/Commands/DeliveryController.cs
@@ -1,4 +1,7 @@
+using KiloMart.Api.Models.Commands;
 using KiloMart.DataAccess.Contracts;
+using KiloMart.Domain.Delivery.Profile.Models;
+using KiloMart.Domain.Delivery.Profile.Services;
 using KiloMart.Domain.Register.Delivery.Models;
 using KiloMart.Domain.Register.Delivery.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -22,19 +25,51 @@ public class DeliveryController : ControllerBase
     public async Task<IActionResult> Register(RegisterDeliveryDto dto)
     {
         var (success, errors) = dto.Validate();
-        
-        if(!success)
+
+        if (!success)
         {
             return BadRequest(errors);
         }
-        
+
         var result = await new RegisterDeliveryService().Register(
             _dbFactory,
             _configuration,
             dto.Email,
-            dto.Password, 
-            dto.DisplayName, 
+            dto.Password,
+            dto.DisplayName,
             dto.Language);
+        return Ok(result);
+    }
+
+    [HttpPost("profile/create")]
+    public async Task<IActionResult> CreateProfile(CreateDeliveryProfileApiRequest request)
+    {
+        var (success, errors) = request.Validate();
+        if (!success)
+        {
+            return BadRequest(errors);
+        }
+
+        var result = await DeliveryProfileService.InsertAsync(_dbFactory,
+        new CreateDeliveryProfileRequest
+        {
+            Delivery = request.Delivery,
+            FirstName = request.FirstName,
+            SecondName = request.SecondName,
+            NationalId = request.NationalId,
+            NationalName = request.NationalName,
+            DrivingLicenseExpiredDate = request.DrivingLicenseExpiredDate,
+            LicenseNumber = request.LicenseNumber,
+            DrivingLicenseNumber = request.DrivingLicenseNumber,
+            LicenseExpiredDate = request.LicenseExpiredDate,
+        }, new CreateDeliveryProfileLocalizedRequest
+        {
+            FirstName = request.FirstName,
+            SecondName = request.SecondName, 
+            Language = request.LanguageId,
+            NationalName = request.NationalName,
+        });
+
         return Ok(result);
     }
 }

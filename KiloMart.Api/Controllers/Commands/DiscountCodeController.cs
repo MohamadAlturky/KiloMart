98125f059bd86e52a33ac30291b5fca7a8ef3c5e
mohamad.alturky <m799678@gmail.com>
2024-11-09 01:15:29 +0300

    adding dicsount code controller

---
+++ KiloMart.Api/Controllers/Commands/DiscountCodeController.cs
@@ -0,0 +1,31 @@
+
+using KiloMart.DataAccess.Contracts;
+using KiloMart.Domain.DiscountCodes.Models;
+using KiloMart.Domain.DiscountCodes.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KiloMart.Api.Controllers.Commands;
+
+[ApiController]
+[Route("api/[controller]")]
+public class DiscountCodeController : ControllerBase
+{
+    private readonly IDbFactory _dbFactory;
+
+    public DiscountCodeController(IDbFactory dbFactory)
+    {
+        _dbFactory = dbFactory;
+    }
+
+    [HttpPost]
+    public IActionResult Create([FromBody] CreateDiscountCodeRequest request)
+    {
+        var result = DiscountCodeService.Insert(_dbFactory, request);
+
+        if (!result.Success)
+            return StatusCode(500,result.Errors);
+
+        return Ok(result.Data);
+
+    }
+}

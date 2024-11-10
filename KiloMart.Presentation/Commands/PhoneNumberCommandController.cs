e74e9b54233d76dc9a9b7340585136893e48d897
mohamad.alturky <m799678@gmail.com>
2024-11-10 03:18:58 +0300

    edit phone number adding action

--- KiloMart.Presentation/Commands/PhoneNumberCommandController.cs
+++ KiloMart.Presentation/Commands/PhoneNumberCommandController.cs
@@ -1,6 +1,9 @@
+using KiloMart.Core.Authentication;
 using KiloMart.Core.Contracts;
 using KiloMart.Domain.PhoneNumbers.Models;
 using KiloMart.Domain.PhoneNumbers.Services;
+using KiloMart.Domain.Register.Utils;
+using KiloMart.Presentation.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace KiloMart.Presentation.Commands;
@@ -10,15 +13,22 @@ namespace KiloMart.Presentation.Commands;
 public class PhoneNumberCommandController : ControllerBase
 {
     private readonly IDbFactory _dbFactory;
-    public PhoneNumberCommandController(IDbFactory dbFactory)
+    private readonly IUserContext _userContext;
+    public PhoneNumberCommandController(IDbFactory dbFactory, IUserContext userContext)
     {
+        _userContext = userContext;
         _dbFactory = dbFactory;
     }
 
     [HttpPost("add")]
+    [Guard([
+        Roles.Customer, 
+        Roles.Provider,
+        Roles.Delivery])]
     public async Task<IActionResult> Add([FromBody] CreatePhoneNumberRequest request)
     {
-        var result = await PhoneNumberService.Insert(_dbFactory, request);
+
+        var result = await PhoneNumberService.Insert(_dbFactory, request,_userContext.Get().Party);
         return result.Success ? Ok(result.Data) : StatusCode(500, result.Errors);
     }
 }

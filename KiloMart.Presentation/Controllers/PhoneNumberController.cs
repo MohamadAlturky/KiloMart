7aa2d0d99f59ef808c394f52c2bdea5d14dcd2f4
mohamad.alturky <m799678@gmail.com>
2024-11-13 20:19:19 +0300

    fixing

--- KiloMart.Presentation/Controllers/PhoneNumberController.cs
+++ KiloMart.Presentation/Controllers/PhoneNumberController.cs
@@ -57,21 +57,6 @@ public class PhoneNumberController : AppController
     }
 
     [HttpGet("mine")]
-    [Guard([Roles.Customer])]
-    public async Task<IActionResult> GetMine()
-    {
-        var partyId = _userContext.Get().Party;
-        using var connection = _dbFactory.CreateDbConnection();
-        connection.Open();
-        var cards = await connection.QueryAsync<CardApiResponse>(
-            "SELECT [Id], [HolderName], [Number], [SecurityCode], [ExpireDate], [Customer] FROM Card WHERE Customer = @partyId AND IsActive = 1;",
-            new { partyId });
-        return Ok(cards.ToArray());
-
-    }
-
-
-    [HttpGet("mine")]
     [Guard([Roles.Customer, Roles.Provider, Roles.Delivery])]
     public async Task<IActionResult> GetByPartyId()
     {

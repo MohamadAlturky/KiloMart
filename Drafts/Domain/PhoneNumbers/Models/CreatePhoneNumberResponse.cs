8181c7a619fc641af84d1618c2e5ec00bd68f541
mohamad.alturky <m799678@gmail.com>
2024-11-13 21:36:24 +0300

    remove drafts

--- Drafts/Domain/PhoneNumbers/Models/CreatePhoneNumberResponse.cs
+++
@@ -1,8 +0,0 @@
-namespace KiloMart.Domain.PhoneNumbers.Models;
-
-public class CreatePhoneNumberResponse
-{
-    public int Id { get; set; }
-    public string Value { get; set; } = string.Empty;
-    public int Party { get; set; }
-}

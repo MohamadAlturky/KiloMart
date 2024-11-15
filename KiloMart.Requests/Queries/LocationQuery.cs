81f6d25e215442a65b5fb6386ad713c840e08c28
Amir2244 <ameryosf224466@gmail.com>
2024-11-15 18:14:53 +0300

    clean LocationController

---
+++ KiloMart.Requests/Queries/LocationQuery.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace KiloMart.Requests.Queries
+{
+    public partial class Query
+    {
+        public static async Task<List<Location>> GetLocationsByParty(IDbConnection connection, int party)
+        {
+            var query = "SELECT * FROM [dbo].[Location] WHERE [Party] = @Party";
+            var result = await connection.QueryAsync<Location>(query, new { Party = party });
+            return result.ToList();
+        }
+    }
+    public class Location
+    {
+        public int Id { get; set; }
+        public float Longitude { get; set; }
+        public float Latitude { get; set; }
+        public string Name { get; set; } = null!;
+        public int Party { get; set; }
+        public bool IsActive { get; set; }
+    }
+}

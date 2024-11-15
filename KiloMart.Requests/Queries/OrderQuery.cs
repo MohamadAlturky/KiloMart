715c9e1e3758cb751ca88c75a8f1967fd0dd284b
Amir2244 <ameryosf224466@gmail.com>
2024-11-15 09:40:47 +0300

    adding GetByProvider and GetByCustomer

--- KiloMart.Requests/Queries/OrderQuery.cs
+++ KiloMart.Requests/Queries/OrderQuery.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Dapper;
+﻿using Dapper;
 using KiloMart.Core.Authentication;
 using KiloMart.Core.Contracts;
 
@@ -12,28 +6,72 @@ namespace KiloMart.Requests.Queries;
 
 public static partial class Query
 {
-    public static async Task<List<Order>> CardById(IDbFactory factory, UserPayLoad payLoad)
+    public static async Task<List<OrderJoinParty>> GetOrderByProvider(IDbFactory factory, UserPayLoad payLoad,
+        int provider)
     {
         using var connection = factory.CreateDbConnection();
         connection.Open();
-        const string query = @"SELECT * FROM dbo.Card where Id=@Id";
+        const string query =
+            @"SELECT p.Id,p.DisplayName,p.IsActive oid FROM [Order] o inner join Party p  on o.Provider=p.Id where o.Provider=@provider";
 
-        var result = await connection.QueryAsync<Order>(query, new
+        var result = await connection.QueryAsync<OrderJoinParty>(query, new
         {
-            Id = 5
+            provider = provider,
         });
         return result.ToList();
     }
 
-    public class Order
+    public static async Task<List<OrderJoinParty>> GetOrderByProviderAndStatus(IDbFactory factory, UserPayLoad payLoad,
+        int provider, bool status)
+    {
+        using var connection = factory.CreateDbConnection();
+        connection.Open();
+        const string query =
+            @"SELECT p.Id,p.DisplayName,p.IsActive oid FROM [Order] o inner join Party p  on o.Provider=p.Id where o.Provider=@provider and o.OrderStatus=@status";
+
+        var result = await connection.QueryAsync<OrderJoinParty>(query, new
+        {
+            provider = provider,
+            status = status,
+        });
+        return result.ToList();
+    }
+
+    public static async Task<List<OrderJoinParty>> GetOrderByCustomer(IDbFactory factory, UserPayLoad payLoad,
+        int customer)
+    {
+        using var connection = factory.CreateDbConnection();
+        connection.Open();
+        const string query =
+            @"SELECT p.Id,p.DisplayName,p.IsActive oid FROM [Order] o inner join Party p  on o.Customer=p.Id";
+
+        var result = await connection.QueryAsync<OrderJoinParty>(query, new
+        {
+            customer = customer,
+        });
+        return result.ToList();
+    }
+
+    public static async Task<List<OrderJoinParty>> GetOrderByCustomerAndStatus(IDbFactory factory, UserPayLoad payLoad,
+        int customer, bool status)
+    {
+        using var connection = factory.CreateDbConnection();
+        connection.Open();
+        const string query =
+            @"SELECT p.Id,p.DisplayName,p.IsActive oid FROM [Order] o inner join Party p  on o.Customer=p.Id and o.OrderStatus=@status";
+
+        var result = await connection.QueryAsync<OrderJoinParty>(query, new
+        {
+            customer = customer,
+            status = status,
+        });
+        return result.ToList();
+    }
+
+    public class OrderJoinParty
     {
         public long Id { get; set; }
-        public byte OrderStatus { get; set; }
-        public decimal TotalPrice { get; set; }
-        public string TransactionId { get; set; } = null!;
-        public int CustomerLocation { get; set; }
-        public int ProviderLocation { get; set; }
-        public int Customer { get; set; }
-        public int Provider { get; set; }
+        public string DisplayName { get; set; }
+        public bool IsActive { get; set; }
     }
 }

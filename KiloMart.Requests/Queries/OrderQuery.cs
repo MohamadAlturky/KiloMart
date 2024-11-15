7b2e4be1d059d852defb64d7f14087d25ac3d53a
Amir2244 <ameryosf224466@gmail.com>
2024-11-15 08:55:21 +0300

    adding OrderQuery

--- KiloMart.Requests/Queries/OrderQuery.cs
+++ KiloMart.Requests/Queries/OrderQuery.cs
@@ -9,26 +9,31 @@ using KiloMart.Core.Authentication;
 using KiloMart.Core.Contracts;
 
 namespace KiloMart.Requests.Queries;
+
 public static partial class Query
 {
+    public static async Task<List<Order>> CardById(IDbFactory factory, UserPayLoad payLoad)
+    {
+        using var connection = factory.CreateDbConnection();
+        connection.Open();
+        const string query = @"SELECT * FROM dbo.Card where Id=@Id";
 
-      public static async Task<List<Card>> CardById(IDbFactory factory, UserPayLoad payLoad)
-      {
-            using var connection = factory.CreateDbConnection();
-            connection.Open();
-            const string query = @"SELECT * FROM dbo.Card where Id=@Id";
+        var result = await connection.QueryAsync<Order>(query, new
+        {
+            Id = 5
+        });
+        return result.ToList();
+    }
 
-            var result = await connection.QueryAsync<Card>(query, new
-            {
-                  Id = 5
-            });
-            return result.ToList();
-
-      }
-
-      public class Card
-      {
-
-      }
+    public class Order
+    {
+        public long Id { get; set; }
+        public byte OrderStatus { get; set; }
+        public decimal TotalPrice { get; set; }
+        public string TransactionId { get; set; } = null!;
+        public int CustomerLocation { get; set; }
+        public int ProviderLocation { get; set; }
+        public int Customer { get; set; }
+        public int Provider { get; set; }
+    }
 }
-

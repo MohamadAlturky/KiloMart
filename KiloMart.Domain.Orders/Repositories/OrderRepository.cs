a0ac033a78f038a69f5776a4dfe909ea6782ccf5
mohamad.alturky <m799678@gmail.com>
2024-11-28 21:58:22 +0300

    get  Orders Activity by id and Orders Filter

---
+++ KiloMart.Domain.Orders/Repositories/OrderRepository.cs
@@ -0,0 +1,102 @@
+using System.Data;
+using Dapper;
+
+namespace KiloMart.Domain.Orders.Repositories;
+
+#region Orders Filter
+public static partial class OrderRepository
+{
+    public static async Task<IEnumerable<OrderDetails>> GetOrderDetailsAsync(IDbConnection connection,
+     string whereClause,
+     object parameters)
+    {
+        var sql = $@"
+            SELECT 
+                o.Id,
+                o.OrderStatus,
+                o.TotalPrice,
+                o.TransactionId,
+                oci.Customer,
+                oci.Location AS CustomerLocation,
+                oci.Id AS CustomerInformationId,
+                opi.Provider,
+                opi.Location AS ProviderLocation,
+                opi.Id AS ProviderInformationId,
+                odi.Delivery,
+                odi.Id AS DeliveryInformationId
+            FROM 
+                dbo.[Order] o
+            LEFT JOIN 
+                dbo.[OrderCustomerInformation] oci ON oci.[Order] = o.Id
+            LEFT JOIN 
+                dbo.[OrderDeliveryInformation] odi ON odi.[Order] = o.Id
+            LEFT JOIN 
+                dbo.[OrderProviderInformation] opi ON opi.[Order] = o.Id
+            {whereClause}
+            ORDER BY 
+                o.[Id];";
+
+        return await connection.QueryAsync<OrderDetails>(sql, parameters);
+    }
+}
+public class OrderDetails
+{
+    public int Id { get; set; }
+    public byte OrderStatus { get; set; }
+    public decimal TotalPrice { get; set; }
+    public string TransactionId { get; set; } = null!;
+
+    public int? Customer { get; set; }
+    public int? CustomerLocation { get; set; }
+    public int? CustomerInformationId { get; set; }
+
+    public int? Provider { get; set; }
+    public int? ProviderLocation { get; set; }
+    public int? ProviderInformationId { get; set; }
+
+    public int? Delivery { get; set; }
+    public int? DeliveryInformationId { get; set; }
+}
+#endregion
+
+
+#region Orders Activity
+public static partial class OrderRepository
+{
+    public static async Task<IEnumerable<OrderActivityDetails>> GetOrderActivitiesAsync(IDbConnection connection, int orderId)
+    {
+        var sql = @"
+                SELECT 
+                    oa.[Id],
+                    oa.[Order],
+                    oa.[Date],
+                    oa.[OrderActivityType],
+                    oat.[Name] AS ActivityTypeName,
+                    oa.[OperatedBy], 
+                    p.[DisplayName] AS OperatedByDisplayName
+                FROM 
+                    dbo.[OrderActivity] oa
+                INNER JOIN 
+                    Party p ON oa.OperatedBy = p.Id
+                INNER JOIN 
+                    OrderActivityType oat ON oat.Id = oa.OrderActivityType
+                WHERE 
+                    oa.[Order] = @OrderId;";
+
+        var parameters = new { OrderId = orderId };
+
+        return await connection.QueryAsync<OrderActivityDetails>(sql, parameters);
+    }
+}
+
+public class OrderActivityDetails
+{
+    public int Id { get; set; }
+    public int Order { get; set; }
+    public DateTime Date { get; set; }
+    public byte OrderActivityType { get; set; }
+    public string ActivityTypeName { get; set; } = null!;
+    public int OperatedBy { get; set; }
+    public string OperatedByDisplayName { get; set; } = null!;
+}
+#endregion

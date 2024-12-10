b1d3f26a97b782c8b0a8a1f416751aa15bda0898
mohamad.alturky <m799678@gmail.com>
2024-12-10 16:19:31 +0300

    adding faq

---
+++ KiloMart.DataAccess/Database/FAQDb.cs
@@ -0,0 +1,115 @@
+using Dapper;
+using System.Data;
+
+namespace KiloMart.DataAccess.Database;
+
+/// <summary>
+/// Table Specification
+/// CREATE TABLE [dbo].[FAQ](
+///     [Id] [int] IDENTITY(1,1) NOT NULL,
+///     [Question] [varchar](200) NOT NULL,
+///     [Answer] [varchar](300) NOT NULL,
+///     [Language] [tinyint] NOT NULL
+/// )
+/// </summary>
+public static partial class Db
+{
+    public static async Task<int> InsertFAQAsync(IDbConnection connection,
+        string question,
+        string answer,
+        byte language,
+        IDbTransaction? transaction = null)
+    {
+        const string query = @"INSERT INTO [dbo].[FAQ]
+                            ([Question], [Answer], [Language])
+                            VALUES (@Question, @Answer, @Language)
+                            SELECT CAST(SCOPE_IDENTITY() AS INT)";
+
+        return await connection.ExecuteScalarAsync<int>(query, new
+        {
+            Question = question,
+            Answer = answer,
+            Language = language
+        }, transaction);
+    }
+
+    public static async Task<bool> UpdateFAQAsync(IDbConnection connection,
+        int id,
+        string question,
+        string answer,
+        byte language,
+        IDbTransaction? transaction = null)
+    {
+        const string query = @"UPDATE [dbo].[FAQ]
+                                SET 
+                                [Question] = @Question,
+                                [Answer] = @Answer,
+                                [Language] = @Language
+                                WHERE [Id] = @Id";
+
+        var updatedRowsCount = await connection.ExecuteAsync(query, new
+        {
+            Id = id,
+            Question = question,
+            Answer = answer,
+            Language = language
+        }, transaction);
+
+        return updatedRowsCount > 0;
+    }
+
+    public static async Task<bool> DeleteFAQAsync(IDbConnection connection, int id, IDbTransaction? transaction = null)
+    {
+        const string query = @"DELETE FROM [dbo].[FAQ]
+                                WHERE [Id] = @Id";
+
+        var deletedRowsCount = await connection.ExecuteAsync(query, new
+        {
+            Id = id
+        }, transaction);
+
+        return deletedRowsCount > 0;
+    }
+
+    public static async Task<FAQ?> GetFAQByIdAsync(int id, IDbConnection connection)
+    {
+        const string query = @"SELECT 
+                            [Id], 
+                            [Question], 
+                            [Answer], 
+                            [Language]
+                            FROM [dbo].[FAQ]
+                            WHERE [Id] = @Id";
+
+        return await connection.QueryFirstOrDefaultAsync<FAQ>(query, new
+        {
+            Id = id
+        });
+    }
+
+    public static async Task<IEnumerable<FAQ>> GetAllFAQsAsync(IDbConnection connection, byte? language = null)
+    {
+        string query = @"SELECT 
+                            [Id], 
+                            [Question], 
+                            [Answer], 
+                            [Language]
+                            FROM [dbo].[FAQ]";
+
+        if (language.HasValue)
+        {
+            query += " WHERE [Language] = @Language";
+            return await connection.QueryAsync<FAQ>(query, new { Language = language.Value });
+        }
+
+        return await connection.QueryAsync<FAQ>(query);
+    }
+}
+
+public class FAQ
+{
+    public int Id { get; set; }
+    public string Question { get; set; } = null!;
+    public string Answer { get; set; } = null!;
+    public byte Language { get; set; }
+}
---
+++ KiloMart.Presentation/Controllers/Shared/FAQController.cs
@@ -0,0 +1,94 @@
+using Microsoft.AspNetCore.Mvc;
+using KiloMart.DataAccess.Database;
+using KiloMart.Core.Contracts;
+using KiloMart.Core.Authentication;
+
+namespace KiloMart.Presentation.Controllers.Shared;
+
+[ApiController]
+[Route("api/faq")]
+public class FAQController : AppController
+{
+    public FAQController(IDbFactory dbFactory, IUserContext userContext)
+        : base(dbFactory, userContext) { }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        using var connection = _dbFactory.CreateDbConnection();
+        connection.Open();
+
+        var faq = await Db.GetFAQByIdAsync(id, connection);
+        if (faq is null)
+        {
+            return DataNotFound();
+        }
+
+        return Success(faq);
+    }
+
+    [HttpGet("list")]
+    public async Task<IActionResult> List([FromQuery] byte? language = null)
+    {
+        using var connection = _dbFactory.CreateDbConnection();
+        connection.Open();
+
+        var faqs = await Db.GetAllFAQsAsync(connection, language);
+
+        return Success(new
+        {
+            Data = faqs,
+            TotalCount = faqs.Count()
+        });
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] FAQDto faqDto)
+    {
+        using var connection = _dbFactory.CreateDbConnection();
+        connection.Open();
+
+        var newId = await Db.InsertFAQAsync(connection, faqDto.Question, faqDto.Answer, faqDto.Language);
+
+        return Success(new { Created = faqDto });
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(int id, [FromBody] FAQDto faqDto)
+    {
+        using var connection = _dbFactory.CreateDbConnection();
+        connection.Open();
+
+        var result = await Db.UpdateFAQAsync(connection, id, faqDto.Question, faqDto.Answer, faqDto.Language);
+
+        if (result)
+        {
+            return Success();
+        }
+
+        return DataNotFound();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        using var connection = _dbFactory.CreateDbConnection();
+        connection.Open();
+
+        var result = await Db.DeleteFAQAsync(connection, id);
+
+        if (result)
+        {
+            return Success();
+        }
+
+        return DataNotFound();
+    }
+}
+
+public class FAQDto
+{
+    public string Question { get; set; } = null!;
+    public string Answer { get; set; } = null!;
+    public byte Language { get; set; }
+}
